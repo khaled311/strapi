5d991b8df6173aa4fd80bf83656d7203b5602a8d
khaled311 <k.sarhan311@gmail.com>
2023-06-02 00:24:11 +0300

    empty

--- config/plugins.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-module.exports = ({ env }) => ({
-  plugins: {
-    "users-permissions": {
-      jwtSecret: crypto.randomBytes(16).toString("base64"),
-    },
-  },
-});
