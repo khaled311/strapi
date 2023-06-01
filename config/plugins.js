675be0f35a829a747c662ca2e3324466eaa0956c
khaled311 <k.sarhan311@gmail.com>
2023-06-02 00:17:45 +0300

    1212

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,7 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_PVapGqj3xtxNaxkFciMlfs5JqfMR/KA5Gb9OSUY",
-      apiToken: "cVpjgH5U7exFgyLZkrtQuqhp",
-      appFilter: "strapi",
-      teamFilter: "team_Blgg6GU6fnoeslxqKDD6SecR",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+  plugins: {
+    "users-permissions": {
+      jwtSecret: crypto.randomBytes(16).toString("base64"),
     },
   },
 });
