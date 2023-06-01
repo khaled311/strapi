ac39a8487d9f967bd2591e01f64e5b1369fb9c05
khaled311 <k.sarhan311@gmail.com>
2023-06-02 00:07:44 +0300

    server file

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "CYvEQx7Oj1hUeLSe3KlIyA==",
+      "tRxZvISpqSDtPLpudbDtYw==",
+      "jSHprEFk2SRTimaHvjR0ow==",
+      "EupWlZemakCZ8hP2fpsTKg==",
+    ],
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
